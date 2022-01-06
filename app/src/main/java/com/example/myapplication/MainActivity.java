ccb923a814d1a18bc548a4bf8f905318b653dc66
dimasarestuprasmanto <dimas.arestu99@gmail.com>
2022-01-06 11:49:26 +0700

    Membuat Web View

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,19 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
     ProgressBar progressBar;
     WebSettings webSettings;
+    Button button;
+    RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.web_view);
         progressBar = findViewById(R.id.progressBar);
-
+        relativeLayout = findViewById(R.id.nointernet);
+        button = findViewById(R.id.reload);
+        relativeLayout.setVisibility(View.GONE);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new myWebViewClient());
@@ -38,8 +48,17 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-            Toast.makeText(getApplicationContext(), "Koneksi Internet Tidak ada", Toast.LENGTH_LONG).show();
-            webView.loadUrl("file:///android_asset/lost.html");
+//            Toast.makeText(getApplicationContext(), "Koneksi Internet Tidak ada", Toast.LENGTH_SHORT).show();
+            relativeLayout.setVisibility(View.VISIBLE);
+            isconnected();
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MainActivity.this.webView.loadUrl(failingUrl);
+                    relativeLayout.setVisibility(View.GONE);
+                }
+            });
         }
 
         @Override
@@ -50,8 +69,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPageFinished(WebView view, String url) {
+            isconnected();
             super.onPageFinished(view, url);
             progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    public void isconnected(){
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED){
+            webView.setVisibility(View.VISIBLE);
+        }else {
+            webView.setVisibility(View.GONE);
         }
     }
 }
